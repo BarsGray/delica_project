197a4c921add7cd30e3d8f8eff4efb5967d582ff
Daniil <m2140sr@gmail.com>
2026-07-22 13:58:44 +0300

    work_d3

--- delica/js/main.js
+++ delica/js/main.js
@@ -1,29 +1,30 @@
-// ======================= menu =============
+// ======================= menu ==========================
 const menuButton = document.querySelector('.menu_btn');
 const svgMenuButton = document.querySelector('.menu_btn .ham');
-// const headerMenu = document.querySelector('.menu_wrap');
-// const overlay = document.querySelector('.overlay');
+const headerMenu = document.querySelector('.header_menu');
+const overlay = document.querySelector('.overlay');
 
 function openMenu() {
-  // document.querySelector('body').classList.toggle('scroll-nane');
+  document.querySelector('body').classList.toggle('scroll-nane');
 
   menuButton.classList.toggle('menu_btn--active');
   svgMenuButton.classList.toggle('active');
-  // headerMenu.classList.toggle('active');
-  // overlay.classList.toggle('visible');
+  headerMenu.classList.toggle('header_menu--active');
+
+  overlay.classList.toggle('overlay--visible');
 }
 
 menuButton.addEventListener('click', openMenu);
-// overlay.addEventListener('click', openMenu);
+overlay.addEventListener('click', openMenu);
 
-// let isExecuted = false;
-// window.addEventListener('resize', () => {
-//   if (window.innerWidth >= 1320) {
-//     if (!isExecuted && headerMenu.classList.contains('active')) {
-//       openMenu();
-//       isExecuted = true;
-//     }
-//   } else {
-//     isExecuted = false;
-//   }
-// });
+let isExecuted = false;
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 1100) {
+    if (!isExecuted && headerMenu.classList.contains('header_menu--active')) {
+      openMenu();
+      isExecuted = true;
+    }
+  } else {
+    isExecuted = false;
+  }
+});
