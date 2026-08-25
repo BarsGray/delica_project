cdd7698f60a9cfd6afab9fed3ca03ab60d2b9efe
Daniil <m2140sr@gmail.com>
2026-08-25 11:57:50 +0300

    day_24

--- delica_th/js/main.js
+++ delica_th/js/main.js
@@ -1,24 +1,14 @@
 jQuery(function ($) {
   // ======================= header =============
-  // const header_top_row_height = document.querySelector('.header_top_row').scrollHeight;
-
   function scrollTop() {
     let scroll_scr = window.scrollY;
-    if (scroll_scr > document.querySelector('.header_top_row').scrollHeight) {
-    // if (scroll_scr > document.querySelector('.header').scrollHeight) {
-    // if (scroll_scr > 101) {
+    if (scroll_scr > document.querySelector('.header_top_row').scrollHeight)
       document.querySelector('.header').classList.add('scroll');
-      // document.querySelector('.header').setAttribute('style','top:-' + document.querySelector('.header_top_row').scrollHeight + 'px;');
-      // document.querySelector('.section_bunner').setAttribute('style','padding-top:' + 0 + 'px;')
-    } else {
+    else
       document.querySelector('.header').classList.remove('scroll');
-      // document.querySelector('.header').setAttribute('style','top:' + 0 + 'px;');
-      // document.querySelector('.section_bunner').setAttribute('style','padding-top:' + header_top_row_height.scrollHeight + 'px;')
-    }
   }
   scrollTop();
   window.addEventListener('scroll', scrollTop);
-
   // ======================= menu ==========================
   const menuButton = document.querySelector('.menu_btn');
   const svgMenuButton = document.querySelector('.menu_btn .ham');
